bb446ced4d472d7299c36a46f52a0b0cd3baa22b
karla ciesielski vida <karlacvida@hotmail.com>
2022-07-10 12:19:49 -0300

    Exercício 1 ao 4

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("teste");
+
+
+
+
+
+
+
+
+
+var subtitulo = documento.create.Element("p");
+subtitulo.innerText = "subtitulo";
+//document.body.appendChild(subtitulo);
+document.body.insertBefore("Li");
+
+
